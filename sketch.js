d892cbbb2cb208a1e4ea1394f013ab52b84c7ada
Bridget Doyle <bridgetdoyle99@gmail.com>
2016-04-19 17:25:21 -0400

    done
    done

--- sketch.js
+++ sketch.js
@@ -1,13 +1,33 @@
-//Feel free to change these words:
-var nouns = ["Hammer","Cat","Tree", "Silence", "Umbrella","Sofa","Shirt","Jacket"];
+var nouns = ["Hello Kitty","Badtz Maru","My Melody", "My Sweet Piano", "Keroppi","Cinnamaroll"];
+var adverbs = ["is","isn't","won't be"];
+var adjectives = ["happy","sad","sweet","funny","evil"];
+var colors = ["blue","green","yellow","black","red","orange"];
+var myVars = [nouns,adverbs,adjectives,colors];
 
 
-function setup() {
+var randNum;
+
+function setup(){
 	createCanvas(windowWidth, windowHeight);
-	
+
+	randNum1 = floor(random(0,adjectives.length));
+	randNum2 = floor(random(0,nouns.length));
+	randNum3 = floor(random(0,adverbs.length));
+	randNum4 = floor(random(0, colors.length));
+
+
 }
 
 function draw(){
-	background("silver");
+	background("pink");
+	textFont("georgia");
+	textSize(60);
+	fill(colors[randNum4]);
+
+	text(nouns[randNum2]+ " "+adverbs[randNum3]+ " "+adjectives[randNum1], 100,200)
+
+
+
+
 
 }
